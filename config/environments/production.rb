73553adeb77915190ad00c100b07eee8868944d3
thiago leite <thiagomoreiraleite@gmail.com>
2021-03-04 20:45:00 -0300

    push form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,6 +112,6 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "agencia-metanoia.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://agencia-metanoia.herokuapp.com/" }
   # or your custom domain name eg. "www.yourdomain.com"
 end
